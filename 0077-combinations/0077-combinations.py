8050684442972195856e844768c6f190a18bab9c
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2023-08-01 18:26:25 +0530

    Time: 44 ms (99.87%), Space: 14.7 MB (86.18%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def combine(self, n, k):
+        result, combination = [], []
+        i = 1
+        while True:
+            if len(combination) == k:
+                result.append(combination[:])
+            if len(combination) == k or \
+               len(combination)+(n-i+1) < k:
+                if not combination:
+                    break
+                i = combination.pop()+1
+            else:
+                combination.append(i)
+                i += 1
+        return result
