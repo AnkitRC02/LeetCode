278b4cafb2121c93ae7252e30c3f0a08ed1320d7
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2023-08-02 20:11:45 +0530

    Time: 19 ms (91.69%), Space: 13.3 MB (88.91%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, num):
+        result = []
+        used = [False] * len(num)
+        self.permuteRecu(result, used, [], num)
+        return result
+
+    def permuteRecu(self, result, used, cur, num):
+        if len(cur) == len(num):
+            result.append(cur[:])
+            return
+        for i in xrange(len(num)):
+            if not used[i]:
+                used[i] = True
+                cur.append(num[i])
+                self.permuteRecu(result, used, cur, num)
+                cur.pop()
+                used[i] = False
