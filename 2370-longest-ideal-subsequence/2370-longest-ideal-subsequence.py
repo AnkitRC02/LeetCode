78ec314d0e4350dd786e261a3c39f68493417a15
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-04-25 09:04:39 +0530

    Time: 778 ms (88.89%), Space: 12.9 MB (100.00%) - LeetHub

---
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -0,0 +1,8 @@
+
+class Solution(object):
+    def longestIdealString(self, s, k):
+        dp = [0]*26
+        for c in s:
+            x = ord(c)-ord('a')
+            dp[x] = max(dp[i] for i in xrange(max(x-k, 0), min(x+k+1, 26)))+1
+        return max(dp)
