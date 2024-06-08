c8e6d57c439cb3875674f21df56f7e938cbd31f5
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-06-08 12:20:02 +0530

    Time: 849 ms (95.71%), Space: 31.1 MB (94.19%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        count = 0
+        lookup = {0: -1}
+        for i, num in enumerate(nums):
+            count += num
+            if k:
+                count %= k
+            if count in lookup:
+                if i - lookup[count] > 1:
+                    return True
+            else:
+                lookup[count] = i
+
+        return False
