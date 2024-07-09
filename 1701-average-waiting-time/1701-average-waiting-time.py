ac60b0403b2f127443a69316d59d8f297c1014e6
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-07-09 13:51:51 +0530

    Time: 689 ms (75.00%), Space: 56.1 MB (80.00%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def averageWaitingTime(self, customers):
+        avai = wait = 0.0
+        for a, t in customers:
+            avai = max(avai, a)+t
+            wait += avai-a
+        return wait/len(customers)
