7dfb48b63a74fe2148fb324b6281b91a51ce9d4d
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-09-27 09:26:09 +0530

    Time: 310 ms (72.09%), Space: 12.2 MB (86.05%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -0,0 +1,16 @@
+class MyCalendarTwo(object):
+
+    def __init__(self):
+        self.__overlaps = []
+        self.__calendar = []
+
+
+    def book(self, start, end):
+        for i, j in self.__overlaps:
+            if start < j and end > i:
+                return False
+        for i, j in self.__calendar:
+            if start < j and end > i:
+                self.__overlaps.append((max(start, i), min(end, j)))
+        self.__calendar.append((start, end))
+        return True
