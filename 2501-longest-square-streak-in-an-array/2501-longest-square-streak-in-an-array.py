526e2b081373faccc92bd2372b8697c9f9da5d2d
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-10-28 11:15:57 +0530

    Time: 79 ms (87.50%), Space: 26.5 MB (53.33%) - LeetHub

---
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestSquareStreak(self, nums):
+        sorted_nums = sorted(set(nums))
+        squares = {x for x in sorted_nums if x%2 < 2}
+        result = 0
+        for x in sorted_nums:
+            square, cnt = x**2, 1
+            while square in squares:
+                squares.remove(square)
+                cnt += 1
+                square *= square
+            result = max(result, cnt)
+        return result if result != 1 else -1
+
