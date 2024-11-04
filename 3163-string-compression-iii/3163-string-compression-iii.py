aef2856df5f241a1e8be58541e18ca43cdd2e1f4
Ankit Roy Chowdhury <132686113+AnkitRC02@users.noreply.github.com>
2024-11-04 11:06:47 +0530

    Time: 415 ms (33.33%), Space: 23.6 MB (27.08%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def compressedString(self, word):
+        result = []
+        cnt = 0
+        for i in xrange(len(word)):
+            cnt += 1
+            if cnt == 9 or (i+1 == len(word) or word[i+1] != word[i]):
+                result.append("%s%s" % (cnt, word[i]))
+                cnt = 0
+        return "".join(result)
